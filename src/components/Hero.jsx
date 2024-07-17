d8857602b06748b92cdf2a5e48ba7f57a0db0b19
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-07-17 03:15:51 +0300

    by meznah

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,36 @@ import React from 'react'
 
 import Lottie from "lottie-react";
 import Main_Animations from "../assets/bgHero2.json";
-
+import heroVideo from '../assets/videoBG.mp4';
+import '../App.css'
 
 const Hero = () => {
   return (
-    <div className='h-[65vh] flex justify-center items-center '>
-      <div className='grid grid-cols-2 w-[100%]'>
-      {/* <svg viewBox="0 0 170 200" xmlns="http://www.w3.org/2000/svg">
-  <path fill="#6E68C4" d="M42.4,-39.7C55.3,-29.4,66.5,-14.7,67.5,1C68.5,16.7,59.3,33.4,46.3,46.5C33.4,59.7,16.7,69.3,2.4,66.9C-12,64.6,-23.9,50.2,-36.6,37.1C-49.3,23.9,-62.7,12,-65.7,-3C-68.7,-17.9,-61.2,-35.9,-48.5,-46.1C-35.9,-56.4,-17.9,-59,-1.6,-57.4C14.7,-55.8,29.4,-49.9,42.4,-39.7Z" transform="translate(100 100)" />
-</svg> */}
+    <div className='h-[90vh] mt-[-1.5%] bg-slate-400 flex justify-center  '>
+  <div className='mainHero border-none relative w-full'>
+    <div className='h-[90vh] bg-black relative overflow-hidden'>
+        <video className='absolute top-0 left-0 min-w-full min-h-full object-cover' autoPlay muted loop>
+            <source src={heroVideo} type="video/mp4" />
+        </video>
+        {/* Overlay color */}
+        <div className="absolute top-0 left-0 w-full h-full bg-[#6e68c4] opacity-50"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+        <h1 className="text-4xl font-bold">نظم معارض التوظيف بسهولة واحترافية</h1>
+        <p className="text-lg">Additional text or description</p>
+    </div>
+    </div>
+
+    <div className="custom-shape-divider-bottom-1721165418">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+        </svg>
+    </div>
+</div>
+
+
+     
+      {/* <div className='grid grid-cols-2 w-[100%]'>
+     
 <div className='mt-[20%] mr-12'>
   <p className='text-[2rem] font-bold text-[#6e68c4] w-[60%]'>نظم معارض التوظيف بسهولة واحترافيه</p>
   <p className='text-[1.2rem] w-[85%] mt-10'>منصة متكاملة لأدارة وتنظيم معارض التوظيف. وتسهيل التواصل بين الشركات والطلاب</p>
@@ -22,7 +43,7 @@ const Hero = () => {
 
 </div>
 
-  </div>
+  </div> */}
     
 
     </div>
